5b9f7f6e0403cb69bfcea6da391837c2c60a9850
Evgeniy Olefir <evgeniy.olefir@gmail.com>
2021-03-16 22:18:01 +0200

    Update MovieDetailsPage.js

--- src/views/MovieDetailsPage.js
+++ src/views/MovieDetailsPage.js
@@ -1,16 +1,24 @@
 import { Component } from 'react';
-import Axios from 'axios';
+
+import axios from 'axios';
 
 class MovieDetailsPage extends Component {
   state = {
     movie: null,
   };
 
-  async componentDidMount() {
-    const response = await Axios.get(
-      'https://developers.themoviedb.org/3/trending/get-trending',
-    );
-    console.log(response);
+  componentDidMount() {
+    axios
+      .get(
+        'https://developers.themoviedb.org/3/trending/all/day?api_key=d0178a81bc9c7d287c5d7ffb12f23888',
+      )
+      .then(response => {
+        console.log(response);
+      });
+    // const response = await Axios.get(
+    //   'https://developers.themoviedb.org/3/trending/get-trending',
+    // );
+    // console.log(response);
   }
 
   render() {
